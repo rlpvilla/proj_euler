3e823ae70a42ff1f2e53382d0f5e3483a38b5a7e
Richie Villa <rlvilla@ucsc.edu>
2013-08-30 04:57:45 -0700

    Syntax Fixed and It's Alive

--- pyth_triplet.py
+++ pyth_triplet.py
@@ -1,12 +1,9 @@
-import math
+from operator import mul
 
 def isPythTriplet (a, b, c):
 	if (a**2 + b**2 == c**2):
 		return True
 
-def tripletSums (a, b, c, sum):
-	if (a + b + c == sum):
-		return True
 
 def findPythTriplet (c):
 	a = 0
@@ -16,15 +13,25 @@ def findPythTriplet (c):
 		for j in range (1, b):
 			a = j
 			if isPythTriplet (a, b, c):
-				return (a, b)
+				return (a, b, c)
+	return False
 
-def locatePythTripletSum (sum):
-	for i in range (1, sum):
-		c = i
-		value = findPythTriplet (c)
-		if tripletSums (*args, c, sum):
-			return (*args, c)
+def pythTripletOfSum (s):
+	g = 0
+	for i in range (1, s):
+		value = findPythTriplet(i)
+		if value != False:
+			if (s % sum(value)) == 0:
+				g = (s/sum(value))
+				return (value[0]*g, value[1]*g, value[2]*g)
+	return False
 
-def productOfTriplet (a, b, c):
-	return (a * b * c)
+def listProduct(list):
+	p = 1
+	for i in list:
+		p *= i
+	return p
 
+s = int(raw_input ('sum = '))
+value = pythTripletOfSum(s)
+print listProduct(value)
